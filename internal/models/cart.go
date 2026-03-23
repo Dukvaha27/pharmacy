44a23a947becf2e44f26855899a793096a129f85
Said-Ahmad <behievs@gmail.com>
2026-03-23 17:04:51 +0300

    add cart

--- internal/models/cart.go
+++ internal/models/cart.go
@@ -4,17 +4,17 @@ import "gorm.io/gorm"
 
 type Cart struct {
 	gorm.Model
-	UserID     int        `json:"user_id" gorm:"not null"`
+	UserID     uint        `json:"user_id" gorm:"not null"`
 	CartItems  []CartItem `json:"cart_items"`
 	TotalPrice int        `json:"total_price"`
 }
 
 type CartCreateRequest struct {
-	UserID    int        `json:"user_id" binding:"required"`
+	UserID    uint        `json:"user_id" binding:"required"`
 	CartItems []CartItem `json:"cart_items" binding:"required"`
 }
 
 type CartUpdateRequest struct {
-	UserID    *int        `json:"user_id"`
+	UserID    *uint        `json:"user_id"`
 	CartItems *[]CartItem `json:"cart_items"`
 }
--- internal/repository/cart_repository.go
+++ internal/repository/cart_repository.go
@@ -1,17 +1,84 @@
 package repository
 
-import "pharmacy/internal/models"
+import (
+	"pharmacy/internal/models"
+
+	"gorm.io/gorm"
+)
 
 type CartRepository interface {
-	GetByUserID(userID uint64) (*models.Cart,error)
+	Create(card *models.Cart) error
+	GetByUserID(userID uint64) (*models.Cart, error)
 
-	AddItem(c *models.CartItem) error
-	
+	AddItem(cardItem *models.CartItem) error
+
 	UpdateItem(item *models.CartItem) error
-	
 	DeleteItem(itemID uint64) error
 	ClearCart(userID uint64) error
 }
 
+type gormCartRepository struct {
+	db *gorm.DB
+}
 
+// func NewCardRepository(db *gorm.DB) CartRepository {
+// 	return &gormCartRepository{db: db}
+// }
 
+func (r gormCartRepository) ClearCart(userID uint64) error {
+	cart, err := r.GetByUserID(userID)
+	if err != nil {
+		return err
+	}
+
+	return r.db.Model(cart).Association("CartItems").Clear()
+
+}
+
+func (r gormCartRepository) DeleteItem(itemID uint64) error {
+	return r.db.Delete(&models.CartItem{}, itemID).Error
+}
+
+func (r gormCartRepository) UpdateItem(item *models.CartItem) error {
+	return r.db.Model(&models.CartItem{}).Where("id = ?", item.ID).Select("*").Updates(item).Error
+}
+
+func (r gormCartRepository) Create(card *models.Cart) error {
+	return r.db.Create(card).Error
+}
+
+func (r gormCartRepository) GetByUserID(userID uint64) (*models.Cart, error) {
+	var cart models.Cart
+	if err := r.db.First(&cart, userID).Error; err != nil {
+		return nil, err
+	}
+	return &cart, nil
+}
+
+func (r gormCartRepository) AddItem(userID uint, cardItem *models.CartItem) error {
+	// получить Cart
+	var card *models.Cart
+	if err := r.db.First(&card, cardItem.CartID).Error; err != nil {
+		card = &models.Cart{
+			UserID: userID,
+		}
+		r.Create(card)
+	}
+	// если Cart нет - создать
+
+	// Получить созданный Cart
+
+	item := models.CartItem{
+		MedicineID: cardItem.MedicineID,
+		Quantity:   cardItem.Quantity,
+		CartID:     card.ID,
+		// <- вроде как не нужен, если мои работаем через Association (тк Cart уже указано)
+		// учесть следующие поля (Quantity, ..., LineTotal)
+	}
+
+	err := r.db.Model(&card).Association("CartItems").Append(&item)
+
+	return err
+}
+
+// func (r gormCartRepository)
